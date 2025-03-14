5a3d3b4aea0a524d0152fb5bd3b3b0ffc5d18e5b
Ayomide Oladele <103321800+Heartz00@users.noreply.github.com>
2025-03-14 10:11:41 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,14 +10,9 @@ function App() {
       <div className="App">
         {/* Navigation Bar */}
         <nav className="navbar">
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Home</Link> {/* Link to the Home/Interview Scheduler page */}
-            </li>
-            <li>
-              <Link to="/admin">Admin</Link> {/* Link to the Admin page */}
-            </li>
-          </ul>
+          <h1 className="nav-links">
+           Pax Interview Scheduler
+          </h1>
         </nav>
 
         {/* Routes */}
